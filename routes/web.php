8dc938dec81fd61ffdda1f41a6d906ce1eed9d63
dongthanhlx <dongthanhlx@gmail.com>
2019-11-04 12:29:42 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+Route::get('welcome', function () {
+    return view('welcome');
+});
 Route::get('/', 'HomeController@index');
 Route::resource('products', 'PageController');
 Route::resource('data', 'MakeDataController');
